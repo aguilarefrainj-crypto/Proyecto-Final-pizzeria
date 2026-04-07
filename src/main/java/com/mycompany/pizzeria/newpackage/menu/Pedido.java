86f06d34ebbee2d704ab764307ded34c4783ee35
efran <efran@DESKTOP-8TI34II>
2026-04-07 12:33:33 -0600

    Commit 3

--- src/main/java/com/mycompany/pizzeria/newpackage/menu/Pedido.java
+++ src/main/java/com/mycompany/pizzeria/newpackage/menu/Pedido.java
@@ -4,6 +4,7 @@
  */
 package com.mycompany.pizzeria.newpackage.menu;
 import com.mycompany.pizzeria.newpackage.Categoria.Categorias;
+import com.mycompany.pizzeria.newpackage.menu.Producto;
 import javax.swing.JOptionPane;
 /**
  *
@@ -37,15 +38,64 @@ public class Pedido {
     }
     
     public static void agregarProductoMenu(){
-        
+        if (contador<producto.length){
+            int codigo = contador + 1;
+            String titulo = JOptionPane.showInputDialog("Nombre del producto: ");
+            double precio = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el precio del producto: "));
+            
+            Categorias categorias [] = Categorias.values();
+            int seleccion = JOptionPane.showOptionDialog(null, "Escoja la categoria del producto: ", "Categorias", 
+                                                        JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, categorias, categorias[0]);
+            Categorias categoria = categorias[seleccion];
+            
+            producto[contador++] = new Producto(codigo, titulo, precio, categoria);
+            JOptionPane.showMessageDialog(null, "Producto Registrado de manera correcta");
+        } else {
+            JOptionPane.showMessageDialog(null, "Capacidad de productos alcanzada");
+        }
     }
     
     public static void editarProductoMenu(){
+        int codigo = Integer.parseInt(JOptionPane.showInputDialog("Digite el codigo a modificar"));
+        Producto productos = null;
         
+        for(int i = 0; i < contador; i++){
+            if(producto[i].getCodigo() == codigo){
+                productos = producto[i];
+                break;
+            }
+        }
+        if(productos!=null){
+            double precio = Double.parseDouble(JOptionPane.showInputDialog("Nuevo precio del producto seleccionado"));
+            productos.setPrecioProducto(precio);
+            JOptionPane.showMessageDialog(null, "Producto actualizado correctamente");
+        }else{
+            JOptionPane.showMessageDialog(null, "No se encontro ningun produccto");
+        }
     }
     
     public static void borrarProductoMenu(){
         
+        int codigo = Integer.parseInt(JOptionPane.showInputDialog("Digite el codigo a buscar"));
+        int indice= -1;
+        
+        for(int  i = 0; i<contador; i++){
+            if(producto[i].getCodigo()==codigo){
+                indice = i;
+                break;
+        }
+      }
+        if(indice ==-1){
+            JOptionPane.showMessageDialog(null, "No se encontro el producto");
+            return;
+        }
+        for(int i = indice; i<contador -1;i++){
+            producto[i]=producto[1];
+        }
+        producto[contador -1 ]= null;
+        contador--;
+        
+        JOptionPane.showMessageDialog(null, "Se borro el producto con exito");
     }
     
     
